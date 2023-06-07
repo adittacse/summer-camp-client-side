48ab6a983d891352a4dbd7305d0cf55cf49c0e8f
Aditta Chakraborty <adittacse@gmail.com>
2023-06-07 21:24:52 +0600

    footer elements updated

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import logo from "../../../assets/TranquilZen.png";
+import moment from 'moment';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className="max-w-screen-xl mx-auto">
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <div>
+                    <img className="w-52" src={logo} alt="Website Footer Logo"/>
+                    <p>Learn YOGA from and MEDITATION <br/>from TranquilZen online!</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Follow Us</span>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Twitter</a>
+                    <a className="link link-hover">LinkedIn</a>
+                    <a className="link link-hover">YouTube</a>
+                </div>
+                <div>
+                    <span className="footer-title">Contact Us</span>
+                    <p>
+                        123 Main St, Suite 456<br />
+                        Paris, France<br />
+                        info@company.com<br />
+                        (123) 456-7890
+                    </p>
+                </div>
+            </footer>
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <div>
+                    <p>Copyright © {moment().format("YYYY")} - All right reserved by ACME Industries Ltd</p>
+                </div>
+            </footer>
         </div>
     );
 };
