78c36dd6d5addce40cc9a44d36bd88fad29f76e0
Aditta Chakraborty <adittacse@gmail.com>
2023-06-07 21:25:31 +0600

    footer elements updated

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
             </footer>
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
                 <div>
-                    <p>Copyright © {moment().format("YYYY")} - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © {moment().format("YYYY")} - All right reserved by TranquilZen</p>
                 </div>
             </footer>
         </div>
