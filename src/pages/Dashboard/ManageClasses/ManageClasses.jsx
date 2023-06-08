82202a4154b7ab0834ae2d14775384e0ad1c9ed5
Aditta Chakraborty <adittacse@gmail.com>
2023-06-09 02:34:47 +0600

    implemented class feedback for admin

--- src/pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import {Helmet} from "react-helmet-async";
+import React, { useRef } from 'react';
+import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle.jsx";
 import useAxiosSecure from "../../../hooks/useAxiosSecure.jsx";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import Swal from "sweetalert2";
 
 const ManageClasses = () => {
+    const modalRefs = useRef({});
+    const textareaRefs = useRef({});
+    
     const [axiosSecure] = useAxiosSecure();
     
-    const {data: classes = [], refetch} = useQuery(["classes"], async () => {
+    const { data: classes = [], refetch } = useQuery(["classes"], async () => {
         const res = await axiosSecure.get("/class");
         return res.data;
     });
@@ -61,6 +64,7 @@ const ManageClasses = () => {
                         'Congratulations!',
                         `${item.className} is Denied`,
                         'success');
+                    handleModal(item);
                 } catch (error) {
                     await Swal.fire({
                         icon: 'error',
@@ -73,12 +77,50 @@ const ManageClasses = () => {
         });
     };
     
+    const handleModal = (item) => {
+        // Open the modal using the showModal() method
+        if (modalRefs.current[item._id]) {
+            modalRefs.current[item._id].showModal();
+        }
+    };
+    
+    const handleSendFeedback = async (item) => {
+        // Retrieve the value from the textarea
+        const feedbackValue = textareaRefs.current[item._id].value;
+        
+        try {
+            await axiosSecure.patch(`/class/feedback/${item._id}`, {
+                feedback: feedbackValue
+            });
+
+            // Close the modal
+            modalRefs.current[item._id]?.close();
+
+            refetch();
+            await Swal.fire(
+                'Congratulations!',
+                'Feedback sent successfully!',
+                'success');
+
+            // Clear the textarea
+            textareaRefs.current[item._id].value = '';
+        } catch (error) {
+            await Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${error.message}`,
+                footer: 'Something went wrong!',
+            })
+        }
+    };
+    
+    
     return (
         <div className="w-[95%] mx-auto">
             <Helmet>
                 <title>Home | TranquilZen</title>
             </Helmet>
-            <SectionTitle heading="Manage Classes"></SectionTitle>
+            <SectionTitle heading="Manage Classes" />
             
             <div className="overflow-x-auto mt-12">
                 <table className="table">
@@ -86,7 +128,7 @@ const ManageClasses = () => {
                     <thead>
                     <tr className="items-center">
                         <th>#</th>
-                        <th>Class Name</th>
+                        <th>Class</th>
                         <th>Available Seats</th>
                         <th>Price</th>
                         <th>Instructor </th>
@@ -95,14 +137,14 @@ const ManageClasses = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    {
-                        classes.map((item, index) => <tr key={item._id}>
+                    {classes.map((item, index) => (
+                        <tr key={item._id}>
                             <th>{index + 1}</th>
                             <td>
                                 <div className="flex items-center space-x-3">
                                     <div className="avatar">
                                         <div className="mask mask-squircle w-12 h-12">
-                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            <img src={item.image} alt="Class image" />
                                         </div>
                                     </div>
                                     <div>
@@ -122,11 +164,23 @@ const ManageClasses = () => {
                             <th className="text-center hover:text-white">
                                 <button onClick={() => handleApproveClass(item)} className="btn btn-ghost btn-xs hover:bg-green-700">Approve</button>
                                 <button onClick={() => handleDenyClass(item)} className="btn btn-ghost btn-xs hover:bg-red-700">Deny</button>
-                                <button className="btn btn-ghost btn-xs hover:bg-secondary">Feedback</button>
+                                <button onClick={() => handleModal(item)} className="btn btn-ghost btn-xs hover:bg-secondary">Feedback</button>
+                                
+                                <dialog ref={ref => modalRefs.current[item._id] = ref} className="modal modal-bottom sm:modal-middle">
+                                    <form method="dialog" className="modal-box">
+                                        <h3 className="font-bold text-lg">Feedback!</h3>
+                                        <textarea ref={ref => textareaRefs.current[item._id] = ref} className="w-full mt-6 p-2" name="feedback" id="feedback" cols="30" rows="10"></textarea>
+                                        <p className="py-4">Press the ESC key or click the button below to close</p>
+                                        <div className="modal-action flex justify-center">
+                                            <input onClick={() => handleSendFeedback(item)} className="btn btn-primary" type="submit" value="Send Feedback" />
+                                            {/* If there is a button in the form, it will close the modal */}
+                                            <button className="btn" onClick={() => modalRefs.current[item._id]?.close()}>Close</button>
+                                        </div>
+                                    </form>
+                                </dialog>
                             </th>
                         </tr>
-                        )
-                    }
+                    ))}
                     </tbody>
                 </table>
             </div>
