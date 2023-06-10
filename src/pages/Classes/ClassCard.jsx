921e443c80755cdda458324e16e61281972841ce
Aditta Chakraborty <adittacse@gmail.com>
2023-06-11 05:08:37 +0600

    dislay counted enrolled students number and available seats in classes page and instructor dashboard

--- src/pages/Classes/ClassCard.jsx
+++ src/pages/Classes/ClassCard.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import useRole from "../../hooks/useRole.jsx";
+import useAxiosSecure from "../../hooks/useAxiosSecure.jsx";
+import Swal from "sweetalert2";
 
 const ClassCard = ({ approvedClass, handleAddToCart }) => {
     const [role] = useRole();
     const {image, className, instructorName, seats, price} = approvedClass;
+    const [axiosSecure] = useAxiosSecure();
+    const [classIdCount, setClassIdCount] = useState(0);
+    const classId = approvedClass._id;
+    const availableSeats = seats - classIdCount;
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosSecure.get(`/payments/count?classId=${classId}`);
+                const { count } = response.data;
+                setClassIdCount(count);
+            } catch (error) {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: `${error.message}`,
+                    footer: 'Something went wrong!',
+                })
+            }
+        };
+        
+        fetchData()
+            .then(() => {})
+            .catch(error => {})
+    }, [axiosSecure, classId]);
+    
     
     // Add a CSS class based on the condition
-    const cardClass = seats == 0 ? "card w-96 bg-base-100 shadow-xl border-4 border-red-500" : "card w-96 bg-base-100 shadow-xl";
+    const cardClass = availableSeats == 0 ? "card w-96 bg-base-100 shadow-xl border-4 border-red-500" : "card w-96 bg-base-100 shadow-xl";
     
     return (
         <div className={cardClass}>
@@ -17,9 +45,10 @@ const ClassCard = ({ approvedClass, handleAddToCart }) => {
                     <div className="badge badge-secondary">${price}</div>
                 </h2>
                 <p className="text-bold text-[17px]">Instructor: {instructorName}</p>
+                <p className="text-bold text-[17px]">Total Seats: {seats}</p>
                 <div className="card-actions justify-between flex items-center mt-4">
                     <div className="badge badge-outline text-[16px] p-4">{
-                        seats < 2 ? `${seats} Seat Available` : `${seats} Seats Available`
+                        availableSeats < 2 ? `${availableSeats} Seat Available` : `${availableSeats} Seats Available`
                     }</div>
                     <button onClick={() => handleAddToCart(approvedClass)} disabled={seats == 0 || role === "Admin" || role === "Instructor"} className="btn btn-primary btn-sm">Select</button>
                 </div>
--- src/pages/Dashboard/MyClasses/MyClasses.jsx
+++ src/pages/Dashboard/MyClasses/MyClasses.jsx
@@ -1,29 +1,67 @@
-import React, {useEffect, useState} from 'react';
-import {Helmet} from "react-helmet-async";
-import SectionTitle from "../../../components/SectionTitle/SectionTitle.jsx";
-import useAxiosSecure from "../../../hooks/useAxiosSecure.jsx";
-import useAuth from "../../../hooks/useAuth.jsx";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle.jsx';
+import useAxiosSecure from '../../../hooks/useAxiosSecure.jsx';
+import useAuth from '../../../hooks/useAuth.jsx';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyClasses = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const [axiosSecure] = useAxiosSecure();
     const [classes, setClasses] = useState([]);
+    const [countEnrolledStudent, setCountEnrolledStudent] = useState([]);
     
-    useEffect( () => {
+    useEffect(() => {
         const fetchDeniedClasses = async () => {
             try {
                 const response = await axiosSecure.get(`/class?instructorEmail=${user.email}`);
                 setClasses(response.data);
             } catch (error) {
-                console.error('Error fetching denied classes:', error);
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: `${error.message}`,
+                    footer: 'Something went wrong!',
+                });
             }
-        }
+        };
         
         fetchDeniedClasses()
-            .then(res => {})
-            .catch(error => {});
+            .then((res) => {})
+            .catch((error) => {});
     }, []);
+    
+    useEffect(() => {
+        if (classes.length > 0) {
+            const currentClassId = classes.map((item) => item._id);
+            const counts = [];
+            
+            const fetchData = async () => {
+                try {
+                    for (let i = 0; i < currentClassId.length; i++) {
+                        const response = await axiosSecure.get(
+                            `/payments/count?classId=${currentClassId[i]}`
+                        );
+                        const { count } = response.data;
+                        counts.push(count);
+                    }
+                    setCountEnrolledStudent(counts);
+                } catch (error) {
+                    await Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: `${error.message}`,
+                        footer: 'Something went wrong!',
+                    });
+                }
+            };
+            
+            fetchData()
+                .then(() => {})
+                .catch((error) => {});
+        }
+    }, [axiosSecure, classes]);
     
     return (
         <div className="w-[95%] mx-auto">
@@ -48,8 +86,8 @@ const MyClasses = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    {
-                        classes.map((item, index) => <tr key={item._id}>
+                    {classes.map((item, index) => (
+                        <tr key={item._id}>
                             <th>{index + 1}</th>
                             <td>
                                 <div className="flex items-center space-x-3">
@@ -65,18 +103,19 @@ const MyClasses = () => {
                                 </div>
                             </td>
                             <td>{item.seats}</td>
-                            <td>{item.enrolledStudent}</td>
-                            <td>{item.seats - item.enrolledStudent}</td>
+                            <td>{countEnrolledStudent[index] || 0}</td>
+                            <td>{item.seats - countEnrolledStudent[index] || 0}</td>
                             <td>{item.status}</td>
                             <td>{item.feedback}</td>
                             <th>
                                 <Link to={`/dashboard/update-class/${item._id}`}>
-                                    <button className="btn btn-ghost btn-xs hover:bg-green-700 hover:text-white">Update Class</button>
+                                    <button className="btn btn-ghost btn-xs hover:bg-green-700 hover:text-white">
+                                        Update Class
+                                    </button>
                                 </Link>
                             </th>
                         </tr>
-                        )
-                    }
+                    ))}
                     </tbody>
                 </table>
             </div>
