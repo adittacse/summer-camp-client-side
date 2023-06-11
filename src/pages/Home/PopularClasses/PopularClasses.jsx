303b2b8768c39b0084e0c28a1fc604a16a34d296
Aditta Chakraborty <adittacse@gmail.com>
2023-06-11 22:09:05 +0600

    made homepage responsive

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -93,7 +93,7 @@ const PopularClasses = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-16">Popular Classes</h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-10 mt-16 mb-16">
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center gap-10 mt-16 mb-16">
                 {
                     topClasses.map(approvedClass => <ClassCard key={approvedClass._id}
                                                                approvedClass={approvedClass}
--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -31,7 +31,7 @@ const PopularInstructors = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-20">Popular Instructors</h2>
-            <div className="grid md:grid-cols-3 gap-10 mt-16 mb-16">
+            <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 mt-16 mb-16 p-4">
                 {
                     instructors.map(instructor => <TopInstructor key={instructor._id}
                                                                   instructor={instructor}></TopInstructor>)
