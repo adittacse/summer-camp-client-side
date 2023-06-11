2c5dd279d5329e690445e7d0f0e0123bf15119d9
Aditta Chakraborty <adittacse@gmail.com>
2023-06-11 22:54:11 +0600

    implemented framer-motion in navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,115 +1,226 @@
-import React, {useEffect, useState} from 'react';
-import logo from "../../../assets/TranquilZen.png";
-import {Link} from "react-router-dom";
-import useAuth from "../../../hooks/useAuth.jsx";
-import useRole from "../../../hooks/useRole.jsx";
-import {FaShoppingCart} from "react-icons/fa";
-import useCart from "../../../hooks/useCart.jsx";
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import logo from '../../../assets/TranquilZen.png';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth.jsx';
+import useRole from '../../../hooks/useRole.jsx';
+import { FaShoppingCart } from 'react-icons/fa';
+import useCart from '../../../hooks/useCart.jsx';
 
 const NavBar = () => {
-    const {user, logOut} = useAuth();
+    const { user, logOut } = useAuth();
     const [role] = useRole();
     const [cart] = useCart();
     
-    // use theme from local storage if available or set light theme
     const [theme, setTheme] = useState(
-        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+        localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
     );
     
-    // update state on toggle
     const handleToggle = (e) => {
         if (e.target.checked) {
-            setTheme("dark");
+            setTheme('dark');
         } else {
-            setTheme("light");
+            setTheme('light');
         }
     };
     
-    // set theme state in localstorage on mount & also update localstorage on state change
     useEffect(() => {
-        localStorage.setItem("theme", theme);
-        const localTheme = localStorage.getItem("theme");
-        // add custom data-theme attribute to html tag required to update theme using DaisyUI
-        document.querySelector("html").setAttribute("data-theme", localTheme);
+        localStorage.setItem('theme', theme);
+        const localTheme = localStorage.getItem('theme');
+        document.querySelector('html').setAttribute('data-theme', localTheme);
     }, [theme]);
     
     const handleLogout = () => {
         logOut()
             .then(() => {})
-            .catch(error => {})
-    }
+            .catch((error) => {});
+    };
     
-    const navMenuItems = <>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/instructors">Instructors</Link></li>
-        <li><Link to="/classes">Classes</Link></li>
-        {
-            role === "Admin" && <>
-                <li><Link to="/dashboard/manage-classes">Dashboard</Link></li>
-            </>
-        }
-        {
-            role === "Instructor" && <>
-                <li><Link to="/dashboard/my-classes">Dashboard</Link></li>
-            </>
-        }
-        {
-            role === "Student" && <>
-                <li><Link to="/dashboard/selected-class">Dashboard</Link></li>
-            </>
-        }
-        {
-            role === "Student" && <li>
-                <Link to="/dashboard/selected-class">
-                    <button className="btn gap-2 text-[16px]">
-                        <FaShoppingCart></FaShoppingCart>
-                        <div className="badge badge-secondary text-[16px]">+{cart?.length || 0}</div>
-                    </button>
-                </Link>
-            </li>
-        }
-        {
-            user && <li><Link onClick={handleLogout} to="/">Logout</Link></li>
-        }
-    </>
+    const navMenuItems = (
+        <>
+            <motion.li
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                key="home"
+            >
+                <Link to="/">Home</Link>
+            </motion.li>
+            <motion.li
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                key="instructors"
+            >
+                <Link to="/instructors">Instructors</Link>
+            </motion.li>
+            <motion.li
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                key="classes"
+            >
+                <Link to="/classes">Classes</Link>
+            </motion.li>
+            {role === 'Admin' && (
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    key="admin-dashboard"
+                >
+                    <Link to="/dashboard/manage-classes">Dashboard</Link>
+                </motion.li>
+            )}
+            {role === 'Instructor' && (
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    key="instructor-dashboard"
+                >
+                    <Link to="/dashboard/my-classes">Dashboard</Link>
+                </motion.li>
+            )}
+            {role === 'Student' && (
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    key="student-dashboard"
+                >
+                    <Link to="/dashboard/selected-class">Dashboard</Link>
+                </motion.li>
+            )}
+            {role === 'Student' && (
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    key="shopping-cart"
+                >
+                    <Link to="/dashboard/selected-class">
+                        <button className="btn gap-2 text-[16px]">
+                            <FaShoppingCart></FaShoppingCart>
+                            <div className="badge badge-secondary text-[16px]">
+                                +{cart?.length || 0}
+                            </div>
+                        </button>
+                    </Link>
+                </motion.li>
+            )}
+            {user && (
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    key="logout"
+                >
+                    <Link onClick={handleLogout} to="/">
+                        Logout
+                    </Link>
+                </motion.li>
+            )}
+        </>
+    );
     
     return (
-        <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
+        <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="navbar bg-base-100 max-w-screen-xl mx-auto"
+        >
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <motion.ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                    >
                         {navMenuItems}
-                    </ul>
+                    </motion.ul>
                 </div>
                 <Link to="/" className="normal-case text-xl">
-                    <img className="w-52" src={logo} alt="Website Logo"/>
+                    <motion.img
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        className="w-52"
+                        src={logo}
+                        alt="Website Logo"
+                    />
                 </Link>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className="navbar-center hidden lg:flex"
+            >
                 <ul className="menu menu-horizontal px-1 items-center">
                     {navMenuItems}
                 </ul>
-            </div>
+            </motion.div>
             <div className="navbar-end">
-                {
-                    user ? <img className="w-16 h-14 mr-2 rounded" src={user?.photoURL} alt="User Proile Picture" />
-                        : <>
-                            <Link className="btn mr-2" to="/login">Login</Link>
-                            <Link className="btn mr-2" to="/signup">Signup</Link>
-                        </>
-                }
-                <button className="btn btn-square btn-ghost">
-                    <label className="swap swap-rotate w-12 h-12">
-                        <input type="checkbox" onChange={handleToggle} checked={theme == "light" ? false : true} />
-                        <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-                        <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-                    </label>
-                </button>
+                {user ? (
+                    <motion.img
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        className="w-16 h-14 mr-2 rounded"
+                        src={user?.photoURL}
+                        alt="User Profile Picture"
+                    />
+                ) : (
+                    <>
+                        <Link className="btn mr-2" to="/login">
+                            Login
+                        </Link>
+                        <Link className="btn mr-2" to="/signup">
+                            Signup
+                        </Link>
+                    </>
+                )}
+                <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="btn btn-square btn-ghost"
+                >
+                    <motion.label
+                        className="swap swap-rotate w-12 h-12"
+                        whileHover={{ rotate: 180 }}
+                    >
+                        <input
+                            type="checkbox"
+                            onChange={handleToggle}
+                            checked={theme === 'light' ? false : true}
+                        />
+                        <motion.svg
+                            className="swap-on fill-current w-10 h-10"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                        </motion.svg>
+                        <motion.svg
+                            className="swap-off fill-current w-10 h-10"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,8,5.36a1,1,0,0,0,0,1.42A8.18,8.18,0,0,1,12,19.46Zm4.49-2.2A6.16,6.16,0,0,0,16,13.77L14.58,15.2a3,3,0,0,1,0,4.24A3.05,3.05,0,0,1,13,20.63a1,1,0,0,0-1.42,1.42A5.06,5.06,0,0,0,13,22.14a5,5,0,0,0,3.54-1.46l1.42-1.42A6.17,6.17,0,0,0,16.49,17.26ZM12.71,8.29A1,1,0,0,0,12,8H7A1,1,0,0,0,7,10h4.59l-1.3,1.29a1,1,0,0,0,1.42,1.42l2-2a1,1,0,0,0,0-1.42l-2-2a1,1,0,0,0-1.42,1.42L11.59,9H7A1,1,0,0,0,7,7h5.18A1,1,0,0,0,12.71,8.29Z" />
+                        </motion.svg>
+                    </motion.label>
+                </motion.button>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
