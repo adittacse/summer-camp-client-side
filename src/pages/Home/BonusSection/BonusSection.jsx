2ecdad2dd69f582229be2db6afa068fea5bbbc77
Aditta Chakraborty <adittacse@gmail.com>
2023-06-12 05:27:27 +0600

    home page design updated

--- src/pages/Home/BonusSection/BonusSection.jsx
+++ src/pages/Home/BonusSection/BonusSection.jsx
@@ -9,7 +9,7 @@ const BonusSection = () => {
             <div className="hero-content bg-base-200 rounded-3xl flex-col lg:flex-row-reverse justify-between px-20">
                 <img src={img} className="max-w-sm rounded-lg shadow-2xl w-1/2" />
                 <div className="w-1/2">
-                    <h1 className="text-5xl font-bold">Discover Inner Harmony: Embrace Yoga and Meditation for Mind, Body, and Soul</h1>
+                    <h1 className="text-3xl font-bold">Embrace Yoga and Meditation for Mind, Body, and Soul</h1>
                     <p className="py-6">Yoga is the journey of the self, through the self, to the self. It is a path of self-discovery, inner peace, and physical and mental well-being. Meditation, on the other hand, is the art of finding stillness and clarity within the chaos of life. Together, yoga and meditation offer a profound means to cultivate balance, harmony, and a deeper connection with oneself and the world around us.</p>
                     <Link to="/classes">
                         <button className="btn btn-warning">See Classes</button>
